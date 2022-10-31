a3733acd21ce7f6b9291a7e057bb2323d82afee8
Marwen.KHAZRI <marwen.khazri@esprit.tn>
2022-10-31 11:31:33 +0400

    test issue fix 11

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -1,5 +1,8 @@
 package tn.esprit.rh.achat;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,5 +14,10 @@ public class AchatApplication {
     public static void main(String[] args) {
         SpringApplication.run(AchatApplication.class, args);
     }
+    
+    @Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
